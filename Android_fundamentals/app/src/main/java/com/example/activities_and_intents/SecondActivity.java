e80e37176e9c6ebc54126e619eeaeab38848eba6
hitanshu_patil <hitanshupatil143@gmail.com>
2022-01-16 16:09:17 +0530

    Unit 2.1 activities and intents completed successfully

--- Android_fundamentals/app/src/main/java/com/example/activities_and_intents/SecondActivity.java
+++ Android_fundamentals/app/src/main/java/com/example/activities_and_intents/SecondActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     TextView textView;
+
+    public static final String EXTRA_REPLY = "com.example.android.activities_and_intents.extra.REPLY";
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,16 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         textView = findViewById(R.id.text_message);
         textView.setText(message);
+
+        mReply = findViewById(R.id.editText_second);
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+
+        finish();
     }
 }
