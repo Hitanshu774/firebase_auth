acb8cebbc2cce0534d8c55b1984d91f4704ee395
Hitanshu <hitanshupatil143@gmail.com>
2022-03-13 00:55:43 +0530

    user registration done successfully

--- temp/firebase_auth/app/src/main/java/com/example/firebase_auth/register.java
+++ temp/firebase_auth/app/src/main/java/com/example/firebase_auth/register.java
@@ -1,5 +1,6 @@
 package com.example.firebase_auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,10 +10,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class register extends AppCompatActivity {
 
     private EditText email1;
     private EditText password1;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +30,33 @@ public class register extends AppCompatActivity {
         email1 = findViewById(R.id.email1);
         password1 = findViewById(R.id.password1);
         Button register2 = findViewById(R.id.register2);
+        auth = FirebaseAuth.getInstance();
 
         register2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(register.this, "hello", Toast.LENGTH_SHORT).show();
-//                String email = email1.getText().toString();
-//                String password = password1.getText().toString();
-//
-//                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
-//                    Toast.makeText(register.this, "Enter email and password!", Toast.LENGTH_SHORT).show();
-//                }
+                String email = email1.getText().toString();
+                String password = password1.getText().toString();
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    Toast.makeText(register.this, "Enter email and password!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    regis(email, password);
+                }
+            }
+        });
+    }
+    private void regis(String email, String password){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(register.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(register.this, "User registered...", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(register.this, "Registration failed!!!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
