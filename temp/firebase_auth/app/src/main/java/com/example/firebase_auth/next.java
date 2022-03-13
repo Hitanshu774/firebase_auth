dd948b745437d8a8363e29798b9bbc3dae9678ab
Hitanshu <hitanshupatil143@gmail.com>
2022-03-13 20:30:04 +0530

    next.java created and linked with xml

--- temp/firebase_auth/app/src/main/java/com/example/firebase_auth/next.java
+++ temp/firebase_auth/app/src/main/java/com/example/firebase_auth/next.java
@@ -3,12 +3,30 @@ package com.example.firebase_auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class next extends AppCompatActivity {
+
+    private EditText name;
+    private EditText age;
+    private Button fill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        name = findViewById(R.id.name);
+        age = findViewById(R.id.age);
+        fill = findViewById(R.id.fill);
+
+        fill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
